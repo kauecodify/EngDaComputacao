69788ef73fa5fc832888f6b8a533af023b13e7ba
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-01-27 20:09:58 -0300

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,5 @@
+# SEMANA 1 - Exercício 1 - Cap. 3 - Ex 4a
+
+celsius = float(input("Digite a temperatura em graus celsius: "))
+fahrenheit = celsius * 9 / 5 + 32
+print(f "A temperatura em fahrenheit é: {fahrenheit:.2f}ºF")
