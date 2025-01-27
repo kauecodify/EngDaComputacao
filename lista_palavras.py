e7a41b31977439931d91b021b569325c9cc9d3b6
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-01-27 20:14:07 -0300

    Add files via upload

---
+++ lista_palavras.py
@@ -0,0 +1,9 @@
+# SEMANA 1 - EXERCÍCIO 2.6 - LISTA DE PALAVRAS
+
+palavras = [ 'taco,', 'bola', 'celeiro', 'cesta', 'peteca']
+
+primeira_palavra = min(palavras) #bola
+ultima_palavra = max(palavra) #taco
+
+print(f"A primeira palavra em ordem alfabética é : {primeira palavra}")
+print(f"A última palavra em ordem alfabética é: {ultima_palavra}")
