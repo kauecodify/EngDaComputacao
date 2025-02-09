bd05c780479660037541682cf56234d926a8b24c
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-02-09 16:06:07 -0300

    Add files via upload

---
+++ ALGORITMO&PROGRAMACAO/exerciciosS2.py
@@ -0,0 +1,35 @@
+# A soma de 2 e 2 é menor que 4
+
+resultado_1 = (2 + 2) < 4
+
+# O valor de 7 // 3 é igual a 1 + 1.
+
+resultado_2 = (7 // 3) == (1 + 1)
+
+# A soma de 3 ao quadrado e 4 ao quadrado é igual a 25.
+
+resultado_3 = (3**2 + 4**2) == 25
+
+# A soma de 2, 4 e 6 é maior que 12.
+
+resultado_4 = (2 + 4 + 6) > 12
+
+# 1387 é divisível por 19
+
+resultado_5 = (1387 % 19 == 0)
+
+# 31 é par.
+
+resultado_6 = (31 % 2 == 0)
+
+# O preço mais baixo dentre R$ 34,99, R$ 29,95 e R$ 31,50 é menor que R$ 30,00.
+
+resultado_7 = min(34.99, 29.95, 31.50) < 30.00
+
+# print(resultado_1)  # False
+# print(resultado_2)  # True
+# print(resultado_3)  # True
+# print(resultado_4)  # False
+# print(resultado_5)  # True
+# print(resultado_6)  # False
+# print(resultado_7)  # True
