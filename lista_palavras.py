5e0251cf26fa635882c6fc34d1f8c37cc17d435f
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-02-09 15:46:33 -0300

    Delete lista_palavras.py

--- lista_palavras.py
+++
@@ -1,9 +0,0 @@
-# SEMANA 1 - EXERCÍCIO 2.6 - LISTA DE PALAVRAS
-
-palavras = [ 'taco,', 'bola', 'celeiro', 'cesta', 'peteca']
-
-primeira_palavra = min(palavras) #bola
-ultima_palavra = max(palavra) #taco
-
-print(f"A primeira palavra em ordem alfabética é : {primeira palavra}")
-print(f"A última palavra em ordem alfabética é: {ultima_palavra}")
