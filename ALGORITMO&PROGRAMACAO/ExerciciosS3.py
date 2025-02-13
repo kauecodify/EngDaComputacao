29a2ab2b6a2dfe2b24d41c6821ff8df2d8d60763
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-02-13 02:18:11 -0300

    Create ExerciciosS3.py

---
+++ ALGORITMO&PROGRAMACAO/ExerciciosS3.py
@@ -0,0 +1,24 @@
+/*avaliando a primeira e a última inf do dataset*/
+
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+sorted(palavras)[0] /*print taco*/
+sorted(palavras)[-1] /*print peteca*/
+
+notas = [9, 7, 7, 10, 3, 9, 6, 6, 2]
+notas.count(7) /*contagem de número de notas*/
+notas[-1] = 4 /*mudando as notas do dataset*/
+max(notas) /*classifica as notas para ordenar notas crescentes e decrescentes*/
+notas.sort() /*cálculo das médias*/
+sum(notas / len(notas) /*somar todos os elementos com sum e dividir com len*/
+
+(a**2 + b**2)**0.5 /*comprimento da hipotenusa (teorema de pitágoras)*/
+
+    (a**2 + b**2)**0.5 ==5 /*avalia se a hipotenusa é igual a 5*/
+
+    3.14159 * a**2 /*verifica se o ponto x, y está dentro de um círculo com centro*/
+
+    /* (x - a)² + (y - b)² <= r² (equação do círculo) */
+
+/*RUN;*/
+
+
